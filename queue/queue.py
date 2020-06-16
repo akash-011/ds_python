df343886ddb10eaa279b7c34f7722d5830287882
Akash <akashbaga011@gmail.com>
2020-06-16 16:13:10 +0300

    add Queue implementation using deque

--- queue/queue.py
+++ queue/queue.py
@@ -14,3 +14,20 @@ queue.pop(0)
 queue.pop(0)
 
 print(f'Queue after dequeing 2 elements {queue}')
+
+#Iplementing a Queue using coleections.deque
+
+from collections import deque
+
+q = deque()
+
+#Add elements using append
+q.append('a')
+q.append('b')
+q.append('c')
+
+print(f'inital queue {q}')
+#dequeue elements using popleft()
+q.popleft()
+q.popleft()
+print(f'Queue after dequeing 2 elements {q}')
