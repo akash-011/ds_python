56ad19425e81f7c4e38b9c1cdb267563cf1004f6
Akash <akashbaga011@gmail.com>
2020-06-19 12:10:19 +0300

    Add Priority Queue Implementation using heapq

---
+++ queue/priority_queue.py
@@ -0,0 +1,12 @@
+#Priority Queue Implementation using heapq(min heapq). heappush used to add elements and heappop used to remove elements in order
+import heapq
+
+my_pq = []
+
+heapq.heappush(my_pq,(3, "Wayne"))
+heapq.heappush(my_pq,(2, "Marcus"))
+heapq.heappush(my_pq,(5, "Phil"))
+heapq.heappush(my_pq,(1, "Paul"))
+
+while my_pq:
+	print(heapq.heappop(my_pq))
