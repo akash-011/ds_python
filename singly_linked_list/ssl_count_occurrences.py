56b1ae1d5dca1c2124afdc5426e889ba63e65b50
Akash <akashbaga011@gmail.com>
2020-06-26 16:05:44 +0300

    ssl count occurrences using iterative function

---
+++ singly_linked_list/ssl_count_occurrences.py
@@ -0,0 +1,24 @@
+from singly_linked_list import LinkedList
+
+def count_occurencces(ssl, data):
+	count = 0 
+	current = ssl.head
+	while current:
+		if current.get_data() == data:
+			 count += 1
+		current = current.get_next()
+
+	return count 
+		
+if __name__ == "__main__":
+	ssl = LinkedList()
+	ssl.append(1)
+	ssl.append(2)
+	ssl.append(1)
+	ssl.append(4)
+	ssl.append(5)
+	ssl.append(1)
+	ssl.append(9)
+	ssl.append(1)
+	print(count_occurencces(ssl, 1))
+	
