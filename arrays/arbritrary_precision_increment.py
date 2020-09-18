25228ce58a680d21e0d4667d4463fd2eb4986905
Akash <akashbaga011@gmail.com>
2020-09-18 17:46:52 +0300

    PEP8 changes

--- arrays/arbritrary_precision_increment.py
+++ arrays/arbritrary_precision_increment.py
@@ -7,19 +7,19 @@ add one to the integer. Assume the solution still works even if implemented in a
 """
 
 def precision_increment(arr):
-	arr[-1] += 1 
-	for i in reversed(range(1, len(arr))):
-		if arr[i] != 10:
-			break
-		arr[i] = 0
-		arr[i-1] += 1
-	
-	if arr[0] == 10:
-		arr[0] = 1
-		arr.append(0)
-	return arr
+    arr[-1] += 1 
+    for i in reversed(range(1, len(arr))):
+        if arr[i] != 10:
+            break
+        arr[i] = 0
+        arr[i-1] += 1
+    
+    if arr[0] == 10:
+        arr[0] = 1
+        arr.append(0)
+    return arr
 
 if __name__ == "__main__":
-	
-	print(precision_increment([9,9,9]))
-	
+    
+    print(precision_increment([9,9,9]))
+    
