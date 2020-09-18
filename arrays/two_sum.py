38c44c6ae0cb2a947f7c978b20a4602bb3477034
Akash <akashbaga011@gmail.com>
2020-09-18 18:18:34 +0300

    two sum brute force method

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -2,3 +2,17 @@
 Problem: Given an array of integers, return indices of the two numbers such that they add up to a specific target. 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 """
+def two_sum_1(arr, target):
+	#brute force approach - n squared
+	for i in range(len(arr)-1):
+		for j in range(i+1 , len(arr)):
+			if arr[i] + arr[j] == target:
+				print(arr[i], arr[j])
+				return True 
+	return False
+
+if __name__ == "__main__":
+	print(two_sum_1([-1,2,5,6,3,10], 13))
+	print(two_sum_1([-1,2,5,6,3,10], 19))
+
+
