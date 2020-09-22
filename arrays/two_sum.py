2461f054f995679c279a20be1e66bf58454d6d8c
Akash <akashbaga011@gmail.com>
2020-09-22 15:20:39 +0300

    two sum with sorted array

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -3,7 +3,7 @@ Problem: Given an array of integers, return indices of the two numbers such that
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 """
 #time complexity - O(n^2)
-#Space complexity - O(n)
+#Space complexity - O(1)
 def two_sum_1(arr, target):
     #brute force approach 
     for i in range(len(arr)-1):
@@ -13,7 +13,8 @@ def two_sum_1(arr, target):
                 return True 
     return False
 
-
+#time complexity - O(n)
+#space complexity - O(n)
 def two_sum_2(arr, target):
     #using a hashtable
     hash_table = {}
@@ -25,6 +26,24 @@ def two_sum_2(arr, target):
             hash_table[target - arr[i]] = arr[i]
     return False       
 
+
+def two_sum_3(arr, target):
+    #array needs to be sorted for this to work
+    i = 0
+    j = len(arr) - 1
+
+    while i <= j:
+        if arr[i] + arr[j] ==  target:
+            print (arr[i], arr[j])
+            return True
+        elif arr[i] + arr[j] < target:
+            i += 1
+        else:
+            j -= 1 
+    
+    return False 
+
+
 if __name__ == "__main__":
-    print(two_sum_2([-1,2,5,6,3,10], 13))
-    print(two_sum_2([-1,2,5,6,3,10], 19))
+    print(two_sum_3([-1,2,3,5,6,10], 13))
+    print(two_sum_3([-1,2,3,5,6,10], 19))
