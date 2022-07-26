759698566ae28bf182bac52dce59af344e7d5e4f
Akash <akashbaga011@gmail.com>
2022-07-26 16:10:53 +0300

    remove element solution 2

--- leetcode/arrays/remove_element.py
+++ leetcode/arrays/remove_element.py
@@ -4,7 +4,19 @@ Given an integer array nums and an integer val, remove all occurrences of val in
 
 # https://leetcode.com/problems/remove-element/
 
+
 def solution_1(nums, val):
+
     while val in nums:
         nums.remove(val)
 
+
+def solution_2(nums, val):
+
+    i = 0
+    for x in nums:
+        if x != val:
+            nums[i] = x
+            i += 1
+
+    return i
