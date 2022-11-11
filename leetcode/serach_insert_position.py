af5fccde58cca5b8ce4434dbf157b7d4c498d7b8
Akash <akashbaga011@gmail.com>
2022-11-11 04:54:32 +0300

    add description for search_insert_position

---
+++ leetcode/serach_insert_position.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode.com/problems/search-insert-position/description/
+st
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
+def serach_insert_postion(nums, target):
+    x =0
+    y = len(nums) - 1
+
+    while x <= y:
+        mid = (x + y) // 2
+        if (nums[mid] == target):
+            return mid
+        elif target > nums[mid]: 
+            x = mid + 1
+        else:
+            y = mid -1 
+
+    return x 
