a8f644bcd87dc283c1a8c125adc090f5da6017b1
Akash <akashbaga011@gmail.com>
2022-11-11 04:51:25 +0300

    add description for perfect number

--- leetcode/postive_integers.py
+++ leetcode/postive_integers.py
@@ -1,3 +1,12 @@
+"""
+https://leetcode.com/problems/perfect-number/description/
+
+A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+
+Given an integer n, return true if n is a perfect number, otherwise return false.
+
+"""
+
 from math import sqrt
 
 
@@ -9,7 +18,6 @@ def check_perfect_number(num):
             postive_divisors.append(i)
             postive_divisors.append(int(num/i))
 
-    print(postive_divisors)
 
     if sum(postive_divisors) == num:
         return True 
