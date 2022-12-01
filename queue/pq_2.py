b826512feb049235968ea7d2c1f8ad8512a2b85c
Akash <akashbaga011@gmail.com>
2022-12-01 15:19:44 +0300

    code formatting

--- queue/pq_2.py
+++ queue/pq_2.py
@@ -1,33 +1,33 @@
 #Priority Queue implementation using lists 
 class PriorityQueue(object):
-	def __init__(self):
-		self.pq = []
-	
-	def is_empty(self):
-		return len(self.pq) == 0
-	
-	def insert(self, data):
-		self.pq.append(data)
-	
-	def remove(self):
-		max = 0 
-		try:
-			for i in range(len(self.pq)):
-				if self.pq[i] > self.pq[max]:
-					max = i
-			item = self.pq[max]
-			del self.pq[max]
-			return item 
-		except IndexError():
-			print()
-			exit()
+    def __init__(self):
+        self.pq = []
+    
+    def is_empty(self):
+        return len(self.pq) == 0
+    
+    def insert(self, data):
+        self.pq.append(data)
+    
+    def remove(self):
+        max = 0 
+        try:
+            for i in range(len(self.pq)):
+                if self.pq[i] > self.pq[max]:
+                    max = i
+            item = self.pq[max]
+            del self.pq[max]
+            return item 
+        except IndexError():
+            print()
+            exit()
 
 if __name__ == "__main__":
-	pq = PriorityQueue() 
-	pq.insert(12) 
-	pq.insert(1) 
-	pq.insert(14) 
-	pq.insert(7) 
-	print(pq)
-	while not pq.is_empty():
-		print(pq.remove())
+    pq = PriorityQueue() 
+    pq.insert(12) 
+    pq.insert(1) 
+    pq.insert(14) 
+    pq.insert(7) 
+    print(pq)
+    while not pq.is_empty():
+        print(pq.remove())
