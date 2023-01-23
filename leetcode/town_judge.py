f1a9a3e7c6b18464577e6d01963ea90cf68f07a1
Akash <akashbaga011@gmail.com>
2023-01-23 16:00:23 +0300

    town judge solution

---
+++ leetcode/town_judge.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/find-the-town-judge
+"""
+
+def findJudge(n, trust):
+    trust_score = [0] * [n+1]
+    for a , b in trust:
+        trust_score[a] -= 1
+        trust_score[b] += 1
+    
+    for i in range(1, n+1):
+        if trust_score[i] == n-1:
+            return i
+    
+    return -1
+
